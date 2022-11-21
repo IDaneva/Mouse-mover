6d2b791be01fda76612e0ae4bda51388feb58c0f
IDaneva <daneva_n@abv.bg>
2022-11-21 11:22:19 +0200

    Update mouse_mover.py

--- mouse_mover.py
+++ mouse_mover.py
@@ -1,9 +1,9 @@
 import random
 import time
-import pyautogui as pag
+import pyautogui
 
 while True:
     x = random.randint(600, 700)
     y = random.randint(200, 600)
-    pag.moveTo(x, y, 0.5)
+    pyautogui.moveTo(x, y, 0.5)
     time.sleep(2)
