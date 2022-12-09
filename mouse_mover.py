7e7e659c31e3c5f98c4fd0b821f89839ef9ab971
IDaneva <daneva_n@abv.bg>
2022-12-09 19:51:10 +0200

    added some GUI

--- mouse_mover.py
+++ mouse_mover.py
@@ -9,19 +9,21 @@ class MyWindow:
     def __init__(self, win):
         self.seconds_entry_lbl = Entry(win, width=7)
         self.seconds_entry_lbl.grid(column=2, row=2, sticky=(W, E))
-        self.move_button = Button(win, text="Move!", command=self.move_mouse).grid(column=3, row=3, sticky=W)
+        self.move_button = Button(win, text="Move!", command=self.process_for_a_given_amount_of_minutes).grid(column=3, row=3, sticky=W)
         self.text_label1 = Label(win, text="Move mouse for ").grid(column=1, row=2, sticky=E)
         self.text_label2 = Label(win, text="minutes").grid(column=3, row=2, sticky=W)
 
-    def move_mouse(self):
-        minutes = self.seconds_entry_lbl.get()
+    @staticmethod
+    def move_mouse():
         x = random.randint(600, 700)
         y = random.randint(200, 600)
         pyautogui.moveTo(x, y, 0.5)
-        #
-        # t_end = time.time() + 60 * int(minutes)
-        # while time.time() < t_end:
-        #     pyautogui.moveTo(x, y, 0.5)
+
+    def process_for_a_given_amount_of_minutes(self):
+        minutes = self.seconds_entry_lbl.get()
+        t_end = time.time() + 60 * int(minutes)
+        while time.time() < t_end:
+            self.move_mouse()
 
 
 root = Tk()
@@ -38,6 +40,6 @@ for child in mainframe.winfo_children():
     child.grid_configure(padx=10, pady=10)
 
 root.mainloop()
-# TODO: make a GUI
+
 # TODO: make it possible to be ran outside the console
 
