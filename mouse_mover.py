2cc3bf6d208939577d44753fdc5e7519347f4d71
IDaneva <daneva_n@abv.bg>
2022-12-10 10:06:20 +0200

    edited some lines of code

--- mouse_mover.py
+++ mouse_mover.py
@@ -9,7 +9,8 @@ class MyWindow:
     def __init__(self, win):
         self.seconds_entry_lbl = Entry(win, width=7)
         self.seconds_entry_lbl.grid(column=2, row=2, sticky=(W, E))
-        self.move_button = Button(win, text="Move!", command=self.process_for_a_given_amount_of_minutes).grid(column=3, row=3, sticky=W)
+        self.move_button = Button(win, text="Move!",
+                                  command=self.process_for_a_given_amount_of_minutes).grid(column=3, row=3, sticky=W)
         self.text_label1 = Label(win, text="Move mouse for ").grid(column=1, row=2, sticky=E)
         self.text_label2 = Label(win, text="minute/s").grid(column=3, row=2, sticky=W)
 
@@ -27,9 +28,9 @@ class MyWindow:
 
 
 root = Tk()
-mywin = MyWindow(root)
+my_window = MyWindow(root)
 root.title('Mouse mover')
-root.geometry("400x250+10+10")
+root.geometry("400x200+10+10")
 
 mainframe = ttk.Frame(root, padding="3 3 12 12")
 mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
@@ -42,4 +43,3 @@ for child in mainframe.winfo_children():
 root.mainloop()
 
 # TODO: make it possible to be ran outside the console
-
